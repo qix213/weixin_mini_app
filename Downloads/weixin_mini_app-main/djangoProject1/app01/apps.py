f0c97fa793d5bec5210d5a8bbed972abc0171c61
duanjiajun <duanjiajun@crrc.com>
2026-02-04 14:21:47 +0800

    feat: add video files with Git LFS

--- Downloads/weixin_mini_app-main/djangoProject1/app01/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App01Config(AppConfig):
-    name = 'app01'
-    verbose_name = '首页欢迎'
--- Downloads/weixin_mini_app-main/miniprogram-1/config/settings.js
+++
@@ -1,9 +0,0 @@
-const rootUrl='http://localhost:8000/app01'
-
-module.exports={
-  welcome: rootUrl + '/welcome/',
-  banner: rootUrl + '/banner/', 
-  category: rootUrl + '/categories/', 
-  goods_detail: rootUrl + '/goods/',
-  base : 'http://localhost:8000',
-}
